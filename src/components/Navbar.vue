7765d17f2c93f3415a780f850ac5734ee70d3de6
Ubuntu <ubuntu@ip-172-26-0-132.ap-southeast-1.compute.internal>
2021-08-20 10:53:43 +0000

    Updates
    Signed-off-by: Ubuntu <ubuntu@ip-172-26-0-132.ap-southeast-1.compute.internal>

---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Courses</li>
+        <li>Login</li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
