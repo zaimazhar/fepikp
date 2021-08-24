f5c930d613edf265c168aa93cafb02cc4cba1b3e
zaimazhar97 <54124001+zaimazhar97@users.noreply.github.com>
2021-08-24 23:51:55 +0800

    Add table to course page
    Signed-off-by: zaimazhar97 <54124001+zaimazhar97@users.noreply.github.com>

--- src/views/Course.vue
+++ src/views/Course.vue
@@ -1,6 +1,13 @@
 <template>
   <v-container>
     <p class="text-center text-h4">Courses</p>
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :items-per-page="5"
+      class="elevation-1"
+    >
+    </v-data-table>
   </v-container>
 </template>
 
